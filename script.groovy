a40f9985c092b7e9b106ff3877208680ec11d8f2
Chetan Patil <chetanpatil06@gmail.com>
2021-09-02 00:48:06 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,1 +1,39 @@
+#!/usr/bin/env groovy
 
+pipeline {
+    agent any
+    tools {
+        maven 'maven'
+    }
+    stages {
+        
+        stage('build app') {
+            steps {
+                script {
+                    echo "building the application..."
+                    sh 'mvn clean package'
+                }
+            }
+        }
+        stage('build image') {
+            steps {
+                script {
+                    echo "building the docker image..."
+                    withCredentials([usernamePassword(credentialsId: 'DockerHub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                        sh "docker build -t chetanpatil06/java-maven:1.3 ."
+                        sh "echo $PASS | docker login -u $USER --password-stdin"
+                        sh "docker push chetanpatil06/java-maven:1.3"
+                    }
+                }
+            }
+        }
+        stage('deploy') {
+            steps {
+                script {
+                    echo 'deploying docker image to EC2...'
+                }
+            }
+        }
+        
+    }
+}
