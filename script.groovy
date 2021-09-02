e5c1d9196723b6712dc42dc7fed5260f007e3198
Chetan Patil <chetanpatil06@gmail.com>
2021-09-02 14:05:43 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,17 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
+def buildJar(){
+                    echo "building the application..."
+                    sh 'mvn clean package'
+                }
+def buildImage(){
+                    echo "building the docker image..."
+                    withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                        sh "docker build -t chetanpatil06/java-maven:1.2 ."
+                        sh "echo $PASS | docker login -u $USER --password-stdin"
+                        sh "docker push chetanpatil06/java-maven:1.2"
+                    }
+                    
+def deployApp(){
+    echo 'deploying the application'
+}
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-    }
-} 
-
-def deployApp() {
-    echo 'deploying the application...'
-} 
-
-return this
+return this 
