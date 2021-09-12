b0957043a92817878a2a98c7f61505eeae092ef2
Cao Cường Nguyễn <codondemen1102@gmail.com>
2021-09-12 22:13:58 +0000

    Update script.groovy

--- Jenkinsfile-syntax/script.groovy
+++ Jenkinsfile-syntax/script.groovy
@@ -1,19 +1,15 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
+def buildApp(){
+    echo 'building the application...'
+}
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-    }
-} 
+def testApp(){
+    echo 'testing the application...'
+}
 
-def deployApp() {
+def deployApp(){
     echo 'deploying the application...'
-} 
+    echo 'deploying version ${params.VERSION}'
+}
 
 return this
+
