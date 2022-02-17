dae8a25e11d13265d308cc07a3831aa83b0bbabe
khalilsliti <khalil.sliti@insat.u-carthage.tn>
2022-02-17 12:51:04 +0000

    Update Jenkinsfile-syntax/script.groovy, Jenkinsfile-syntax/Jenkinsfile

--- Jenkinsfile-syntax/script.groovy
+++ Jenkinsfile-syntax/script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'docker_credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t pattaclope/java-mvn-app:1.2 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push pattaclope/java-mvn-app:1.2'
     }
 } 
 
