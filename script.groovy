a97ef5546fbc93feb1bd1312df5b88f7a7085460
khalilsliti <khalil.sliti@insat.u-carthage.tn>
2022-02-17 23:08:46 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker_credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t pattaclope/java-mvn-app:1.2 .'
+        sh "docker build -t pattaclope/java-mvn-app:$VERSION ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push pattaclope/java-mvn-app:$VERSION"
     }
