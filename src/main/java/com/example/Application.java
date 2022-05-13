6ba0d637b18c8dccc143296c5443d3e9ab188829
Erfan Motallebi <Erfan.Motallebi1990@gmail.com>
2022-05-13 16:13:02 +0430

    last changed

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,22 +1,21 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @PostConstruct
-    public void init()
-    {
+    public void init() {
         Logger log = LoggerFactory.getLogger(Application.class);
         log.info("Java app started");
     }
