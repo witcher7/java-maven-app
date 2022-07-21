88d6ff3d08158d0f4d604c008fe1a5a1a1fcc6fa
adeolad1 <adeola_dolapo@yahoo.com>
2022-07-21 00:37:14 +0000

    Update Jenkinsfile-simple-pipeline/script.groovy

--- Jenkinsfile-simple-pipeline/script.groovy
+++ Jenkinsfile-simple-pipeline/script.groovy
@@ -1,19 +1,14 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
+def buildApp() {
+    echo 'building the application...'
+}
 
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
-    }
-} 
+def testApp() {
+    echo 'building it to test ......'
+}
 
 def deployApp() {
-    echo 'deploying the application...'
-} 
+    echo 'building for deployment ......'
+    // echo "deployin version ${params.VERSION}"
+}
 
 return this
