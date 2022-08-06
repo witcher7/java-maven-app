06956ae2be159a013f28b24f169208a4a4b6a77f
Dave Leong <wenghong1227@gmail.com>
2022-08-06 15:06:28 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,12 +1,13 @@
 def buildApp() {
-    echo "building the application..."
+    echo 'building the application...'
 } 
 def testApp() {
-    echo "building the application..."
+    echo 'building the application...'
     }
 } 
 def deployApp() {
     echo 'deploying the application...'
     echo "deploying version ${params.VERSION}"
 } 
+
 return this
