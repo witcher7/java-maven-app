124422355480f0106b391b04b002188618e101e6
Dave Leong <wenghong1227@gmail.com>
2022-08-06 14:51:26 +0000

    Add new file

---
+++ script.groovy
@@ -0,0 +1,12 @@
+def buildJar() {
+    echo "building the application..."
+} 
+def buildImage() {
+    echo "building the application..."
+    }
+} 
+def deployApp() {
+    echo 'deploying the application...'
+    echo "deploying version ${params.VERSION}"
+} 
+return this
