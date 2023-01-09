07000459f2fb707f213c1ea07c3e65b849a2b3ea
Yasser Magdy <yassermagdy430@gmail.com>
2023-01-09 16:54:05 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar () {
 
 def buildImage () {
     echo "building the docker image......"
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo	',passwordVariable: 'PASS',usernameVariable:'USER')]){
+    withCredentials([usernamePassword(credentialsId: 'docker-hub	',passwordVariable: 'PASS',usernameVariable:'USER')]){
         sh 'docker build -t yasser1133/demo-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push yasser1133/demo-app:jma-2.0'
