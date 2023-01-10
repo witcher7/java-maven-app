76e41554bf1708946d3c733d30477f60fdd99170
Yasser Magdy <yassermagdy430@gmail.com>
2023-01-10 16:06:26 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,24 @@
+def incrementVersion () {
+    echo "incrementing app version......"
+    sh 'mvn build-helper:parse-version versions:set \
+    -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+    versions:commit'
+    def matcher = readFile('pom.xml') =~ '<version>(.+)<version>'
+    def version = matcher[0][1]
+    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
+}
+
 def buildJar () {
     echo "building the application....."
-    sh 'mvn package'
+    sh 'mvn clean package'
 }
 
 def buildImage () {
     echo "building the docker image......"
     withCredentials([usernamePassword(credentialsId: 'docker-hub	',passwordVariable: 'PASS',usernameVariable:'USER')]){
-        sh 'docker build -t yasser1133/demo-app:jma-2.0 .'
+        sh "docker build -t yasser1133/demo-app:${IMAGE_NAME} ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push yasser1133/demo-app:jma-2.0'
+        sh 'docker push yasser1133/demo-app:${IMAGE_NAME}'
     }
 }
 
