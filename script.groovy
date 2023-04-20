5624798bbc612ea46be2b1f2211df4c27ffb7ec8
sabry Elhasanin <sabryelhasanin@gmail.com>
2023-04-20 07:41:21 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,15 +2,7 @@ def buildJar() {
     echo "building the application..."
     sh 'mvn package'
 } 
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t sabryelhasanin/demo-app:jma-1.0 .'
-        sh "echo $PASSWORD| docker login -u $USERNAME --password-stdin"
-        sh 'docker push sabryelhasanin/demo-app:jma-1.0'
-    }
-} 
+ 
 
 def deployApp() {
     echo 'deploying the application...'
