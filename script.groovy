b28a2ecc9ee9a10ec9b2ae18f2406ba6ae53b8cf
bharatmundra <bharatmundra119@gmail.com>
2024-05-25 10:06:57 +0000

    adding groovy script for  pipeline

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp()
+{
+    echo "builing the application"
+}
+def testApp()
+{
+    echo "Testing the application"
+}
+def deployApp()
+{
+    echo "deploying the application"
+}
+return this
