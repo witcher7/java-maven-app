d57bfd4ee64dfdb5acb43cbe5025513a92a6da33
bharatmundra <bharatmundra119@gmail.com>
2024-05-25 15:43:18 +0000

    changes in script and jenkin file

--- script.groovy
+++ script.groovy
@@ -1,11 +1,21 @@
-def buildApp()
+def build_Jar()
 {
-    echo "builing the application"
+    echo " building the application"
+    sh "mvn package"
 }
-def testApp()
+
+def buildImage()
 {
-    echo "Testing the application"
+    echo " building the docker  image..."
+    withCredentials([usernamePassword(credentialsId:'docker-hub-repo', usernameVariable:'USER' ,passwordVariable: 'PASS')])
+    {
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker build -t bharatmundra/demo-app:1.5.1 . "
+        sh "docker push bharatmundra/demo-app:1.5.1 "
+    }
+             
 }
+
 def deployApp()
 {
     echo "deploying the application"
